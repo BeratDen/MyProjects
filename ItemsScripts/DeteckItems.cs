19dec6d4ed88fd8deed9d6126522fd20cba739af
Berat Deniz <70016159+BeratDen@users.noreply.github.com>
2021-03-09 17:21:36 +0300

    Unity C# Item Deteck Scrip

---
+++ ItemsScripts/DeteckItems.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteckItems : MonoBehaviour
+{
+    PlayerHP playerHP;
+    private void Start()
+    {
+        playerHP = GetComponent<PlayerHP>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HealtItem"))
+        {
+            playerHP.AddHP(20);
+            other.gameObject.SetActive(false);
+            print("20 can eklendi" + playerHP.GetHealth());
+        } 
+    }
+
+}
